cc8cc5f4fe8c8a318d423bd84792ebcbfda3d50c
Nhat Anh <ltnanh21@gmail.com>
2024-07-18 11:20:15 +0700

    Create backend folder and add dependencies to pom.xml

---
+++ backend/project-management-system/src/main/java/com/_anhn/ProjecetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com._anhn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecetManagementSystemApplication.class, args);
+	}
+
+}
