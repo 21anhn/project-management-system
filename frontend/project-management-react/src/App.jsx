9d35286fd2f5be3ce2d85a88263d33b19e402f84
Nhat Anh <ltnanh21@gmail.com>
2024-07-20 15:15:33 +0700

    Crate Home Page UI using Reactjs + TailwindCSS + Vite

---
+++ frontend/project-management-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+import Home from './pages/Home/Home'
+import Navbar from './pages/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
